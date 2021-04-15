50772dc09d452e719da0606c38d320db750ac4e3
Williams Gutierrez <wroger.gutierrez@gmail.com>
2021-04-14 21:52:03 -0400

    Fix ip services box pc

--- src/services/ConfigServices.js
+++ src/services/ConfigServices.js
@@ -2,14 +2,14 @@
  * Created by Williams on 31/03/2021
  */
 const protocol = "http";
-const host = "192.168.0.100";
+const host = "192.168.0.109";
 const portApiServices = "8011";
 
 const portReportServices = "80";
+
 const reportServicesName = "PrograTres";
 
-export const API_SERVICES =
-  protocol + "://" + host + ":" + portApiServices + "/";
+export const API_SERVICES = protocol + "://" + host + ":" + portApiServices;
 
 export const REPORT_SERVICES =
   protocol +
